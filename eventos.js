feea2e3fe6278b4d129aff7498fe2f5215b0c23c
cacovidigal <marcosmsvidigal@gmail.com>
2023-04-15 09:10:08 -0300

    elaborando função js

--- eventos.js
+++ eventos.js
@@ -1,12 +1,27 @@
-function Enviar() {
 
+
+    //variaveis do formulario
     var atividade = document.getElementById('atividadeid')
     var dataInicio = document.getElementById('dataInicioid')
     var dataFim = document.getElementById('dataFimid')
     var Status = document.getElementById('Statusid')
+    //variaveis da tabela
+    var tabAtividade = document.getElementById('atividade1')
+    var tabInicio = document.getElementById('inicio1')
+    var tabFim = document.getElementById('fim1')
+    var tabStatus = document.getElementById('resultado1')
+    //função para levar do campo formulario até o campo da tabela
 
-    atividade.innerHTML =  atividade1
-    dataInicio.innerHTML = inicio1
-    dataFim.innerHTML = fim1
-    Status.innerHTML = resultado1
+    tabAtividade.innerHTML =  atividade
+    tabInicio.innerHTML = dataInicio
+    tabFim.innerHTML = dataFim
+    tabStatus.innerHTMl = Status
+
+    /*
+    atividade.innerHTML = tabAtividade 
+    dataInicio.innerHTML = tabInicio
+    dataFim.innerHTML = tabFim
+    Status.innerHTML = tabStatus*/
+    function Enviar() {
+        if
 }
