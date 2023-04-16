29a2ba72b1b72744b57259b775a8a74bb039a78f
cacovidigal <marcosmsvidigal@gmail.com>
2023-04-16 16:30:20 -0300

    a resolver

--- eventos.js
+++ eventos.js
@@ -1,7 +1,7 @@
 
 
     //variaveis do formulario
-    var atividade = document.getElementById('atividadeid')
+    var atividade = document.getElementById('atividadeid') 
     var dataInicio = document.getElementById('dataInicioid')
     var dataFim = document.getElementById('dataFimid')
     var Status = document.getElementById('Statusid')
@@ -12,16 +12,20 @@
     var tabStatus = document.getElementById('resultado1')
     //função para levar do campo formulario até o campo da tabela
 
+    //console.log({atividade})
+    
+
+
+    /*
     tabAtividade.innerHTML =  atividade
     tabInicio.innerHTML = dataInicio
     tabFim.innerHTML = dataFim
     tabStatus.innerHTMl = Status
+    */
 
     /*
     atividade.innerHTML = tabAtividade 
     dataInicio.innerHTML = tabInicio
     dataFim.innerHTML = tabFim
     Status.innerHTML = tabStatus*/
-    function Enviar() {
-        if
-}
+    
